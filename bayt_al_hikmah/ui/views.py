105686575a064b1c80c0eb476bdc6881de741399
Yousef Abu Shanab <93343012+youzarsiph@users.noreply.github.com>
2025-05-15 06:32:29 -0700

    add registration views

--- bayt_al_hikmah/ui/views.py
+++ bayt_al_hikmah/ui/views.py
@@ -1,10 +1,66 @@
 """Views for bayt_al_hikmah.tags"""
 
-from django.views.generic import TemplateView
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views import generic
+
+from bayt_al_hikmah.ui import mixins
+from bayt_al_hikmah.ui.forms import UserCreateForm
 
 
 # Create your views here.
-class HomeView(TemplateView):
+class HomeView(generic.TemplateView):
     """Home page"""
 
     template_name = "ui/index.html"
+
+
+# Auth views
+User = get_user_model()
+
+
+class ProfileView(LoginRequiredMixin, generic.TemplateView):
+    """Profile page"""
+
+    template_name = "registration/profile.html"
+
+
+class SignupView(SuccessMessageMixin, generic.CreateView):
+    """Create a new user"""
+
+    model = User
+    form_class = UserCreateForm
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy("ui:profile")
+    success_message = "Your account was created successfully!"
+
+
+class UserUpdateView(
+    LoginRequiredMixin,
+    SuccessMessageMixin,
+    mixins.AccountOwnerMixin,
+    generic.UpdateView,
+):
+    """Update a user"""
+
+    model = User
+    template_name = "registration/form.html"
+    fields = ["first_name", "last_name", "email"]
+    success_url = reverse_lazy("ui:profile")
+    success_message = "Your account was updated successfully!"
+
+
+class UserDeleteView(
+    LoginRequiredMixin,
+    SuccessMessageMixin,
+    mixins.AccountOwnerMixin,
+    generic.DeleteView,
+):
+    """Delete a user"""
+
+    model = User
+    template_name = "registration/form.html"
+    success_url = reverse_lazy("ui:index")
+    success_message = "Your account was deleted successfully!"
