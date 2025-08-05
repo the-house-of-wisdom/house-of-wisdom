f735d18644c6f8d597ae3ce6e6cae989ce4dc58c
Yousef Abu Shanab <93343012+youzarsiph@users.noreply.github.com>
2025-08-05 01:24:46 -0700

    update ui

--- how/cms/wagtail_hooks.py
+++ how/cms/wagtail_hooks.py
@@ -7,8 +7,6 @@ from wagtail.admin.panels import FieldPanel
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.snippets import SnippetViewSet
 
-from how.apps.categories.models import Category
-
 
 class TagsSnippetViewSet(SnippetViewSet):
     """
